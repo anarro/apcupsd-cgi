4121d9198fdb642a455f06f6611c85069c7b9a00
Antoni <antoni.artigas@gmail.com>
2017-04-23 21:44:55 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-sed -i "s/UPSCABLE smart/UPSCABLE usb/g" /etc/apcupsd/apcupsd.conf
+#sed -i "s/UPSCABLE smart/UPSCABLE usb/g" /etc/apcupsd/apcupsd.conf
+#sed -i "s/NISIP 127.0.0.1/NISIP 0.0.0.0/g" /etc/apcupsd/apcupsd.conf
+#sed -i "s/UPSTYPE apcsmart/UPSTYPE usb/g" /etc/apcupsd/apcupsd.conf
+#sed -i "s/DEVICE \/dev\/ttyS0/DEVICE/g" /etc/apcupsd/apcupsd.conf
+
+sed -i "s/UPSCABLE smart/UPSCABLE ether/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/NISIP 127.0.0.1/NISIP 0.0.0.0/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/UPSTYPE apcsmart/UPSTYPE usb/g" /etc/apcupsd/apcupsd.conf
-sed -i "s/DEVICE \/dev\/ttyS0/DEVICE/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/UPSTYPE apcsmart/UPSTYPE snmp/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/DEVICE \/dev\/ttyS0/DEVICE 192.168.1.11:161:APC:private/g" /etc/apcupsd/apcupsd.conf
 
 apcupsd -b;
